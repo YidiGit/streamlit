017fc1ca2f96b92ea8a0ff7be773503cdbaf2e72
YidiGit <yidiqiao0609@163.com>
2025-03-15 14:08:02 +0000

    Update sketch_streamlit2.py

--- sketch_streamlit2.py
+++ sketch_streamlit2.py
@@ -110,7 +110,7 @@ def main():
         unsafe_allow_html=True
     )
 
-    col_left, col_right = st.columns(2)
+    col1 , col_left, col_right = st.columns([1,5,4])
     
     with col_left:
         st.write("**Draw your sketch below to get started:**")
